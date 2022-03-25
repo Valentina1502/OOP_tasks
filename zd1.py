7cca581cc1b46f40af0bc5a02d181a1b9098d0bd
Valentina1502 <valan2529@gmail.com>
2022-03-25 14:04:56 +0300

    edit

--- zd1.py
+++ zd1.py
@@ -4,11 +4,11 @@
 '''
 Реализация алгоритма Хаффмана
 '''
+
 from collections import Counter
 
 
 class Node:
-
     def __init__(self, value, left=None, right=None):
         self.right = right
         self.left = left
@@ -16,17 +16,14 @@ class Node:
 
 
 def get_code(root, codes=dict(), code=''):
-
     if root is None:
         return
-
     if isinstance(root.value, str):
         codes[root.value] = code
         return codes
 
     get_code(root.left, codes, code + '0')
     get_code(root.right, codes, code + '1')
-
     return codes
 
 
@@ -35,11 +32,9 @@ def get_tree(string):
 
     if len(string_count) <= 1:
         node = Node(None)
-
         if len(string_count) == 1:
             node.left = Node([key for key in string_count][0])
             node.right = Node(None)
-
         string_count = {node: 1}
 
     while len(string_count) != 1:
@@ -48,48 +43,37 @@ def get_tree(string):
 
         if isinstance(spam[0][0], str):
             node.left = Node(spam[0][0])
-
         else:
             node.left = spam[0][0]
-
         if isinstance(spam[1][0], str):
             node.right = Node(spam[1][0])
-
         else:
             node.right = spam[1][0]
 
         del string_count[spam[0][0]]
         del string_count[spam[1][0]]
         string_count[node] = spam[0][1] + spam[1][1]
-
     return [key for key in string_count][0]
-
 
 def coding(string, codes):
     res = ''
-
     for symbol in string:
         res += codes[symbol]
-
     return res
-
 
 def decoding(string, codes):
     res = ''
     i = 0
-
     while i < len(string):
-
         for code in codes:
-
             if string[i:].find(codes[code]) == 0:
                 res += code
                 i += len(codes[code])
-
     return res
 
+
 if __name__ == '__main__':
-    my_string = input('Введите строку для сжатия: ')
+    my_string = input('Введите строку: ')
     tree = get_tree(my_string)
 
     codes = get_code(tree)
--- zd2.py
+++ zd2.py
@@ -3,15 +3,15 @@
 
 
 '''
-Реализация алгоритма Хаффмана
+Реализация задачи про максимальную стоимость предметов рюкзака
 '''
 
 
-def knapsack(cap, values, weights):
+def knapsack(bag, goods, weights):
     items = []
-    for i in range(len(values)):
+    for i, item in enumerate(goods):
         itemInfo = {
-            'vpw': values[i] / weights[i],
+            'vpw': goods[i] / weights[i],
             'weight': weights[i]
         }
         if len(items) == 0:
@@ -22,22 +22,19 @@ def knapsack(cap, values, weights):
                 k += 1
             items.insert(k, itemInfo)
     total = 0
-    cap_left = cap
+    bag_left = bag
     for item in items:
-        if cap_left - item['weight'] >= 0:
+        if bag_left - item['weight'] >= 0:
             total += item['weight'] * item['vpw']
-            cap_left -= item['weight']
-        elif cap_left > 0:
-            total += item['vpw'] * cap_left
-            cap_left = 0
+            bag_left -= item['weight']
+        elif bag_left > 0:
+            total += item['vpw'] * bag_left
+            bag_left = 0
     return total
 
 
 if __name__ == '__main__':
-    #Общая вместительность рюкзака
-    cap = 40
-    #Стоимость предметов
-    values = [50, 75, 100]
-    #соответствующие веса предметов
-    weights = [10, 20, 15]
-    print("Максимальная стоимость рюкзака составит: ", knapsack(cap, values, weights))
+    bag = int(input("Вместимость рюкзака: "))
+    goods = tuple(map(int, input("Введите соответствующую стоимость предметов:  ").split()))
+    weights = tuple(map(int, input("Введите соответствующие веса предметов: ").split()))
+    print("\nМаксимальная стоимость рюкзака составит: ", knapsack(bag, goods, weights))
